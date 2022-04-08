022534650f7e9ef17e4ba5d593c2c7d28304da6c
Zholdas Ramazan <ghostcomrad@gmail.com>
2022-04-08 19:28:45 +0600

    adding controllers

--- main.go
+++ main.go
@@ -1,21 +1,15 @@
 package main
 
 import (
-	"encoding/base64"
 	"fmt"
 	"log"
-	"net/http"
 	"os"
-	"strconv"
 
+	"github.com/RamazanZholdas/MyGoPlayground/jwtGinGolangTest/controllers"
 	"github.com/RamazanZholdas/MyGoPlayground/jwtGinGolangTest/database"
-	"github.com/RamazanZholdas/MyGoPlayground/jwtGinGolangTest/structs"
-	"github.com/RamazanZholdas/MyGoPlayground/jwtGinGolangTest/tokens"
-	"go.mongodb.org/mongo-driver/bson"
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
-	"golang.org/x/crypto/bcrypt"
 )
 
 var (
@@ -23,7 +17,7 @@ var (
 	mongoUri       string
 	dbName         string
 	collectionName string
-	bcryptCost     int
+	envData        map[string]string
 )
 
 func init() {
@@ -37,7 +31,7 @@ func init() {
 		log.Fatal("BCRYPT_COST not found, using minimum cost: 4")
 		bcryptCostStr = "4"
 	}
-	bcryptCost, _ = strconv.Atoi(bcryptCostStr)
+	envData["BCRYPT_COST"] = bcryptCostStr
 
 	port, ok = os.LookupEnv("PORT")
 	if !ok {
@@ -56,13 +50,14 @@ func init() {
 		fmt.Println("DB_NAME not found, using default: test")
 		dbName = "test"
 	}
+	envData["DB_NAME"] = dbName
 
 	collectionName, ok = os.LookupEnv("COLLECTION_NAME")
 	if !ok {
 		fmt.Println("COLLECTION_NAME not found, using default: users")
 		collectionName = "users"
 	}
-
+	envData["COLLECTION_NAME"] = collectionName
 }
 
 func main() {
@@ -80,93 +75,8 @@ func main() {
 
 	c := gin.Default()
 	// http://localhost:8080/{paste guid here}
-	c.GET("/:guid", func(c *gin.Context) {
-		c.Writer.Header().Add("Content-Type", "application/json")
-		var user structs.User
-
-		token, refreshToken, jti, err := tokens.GenerateTokens(c.Param("guid"))
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-			return
-		}
-
-		hash, err := bcrypt.GenerateFromPassword([]byte(refreshToken), bcryptCost)
-		if err != nil {
-			log.Fatal(err)
-		}
-		user.GUID = c.Param("guid")
-		user.RefreshToken = string(hash)
-		user.Jti = jti
-
-		insertOneResult, err := database.InsertOne(client, ctx, dbName, collectionName, user)
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-			fmt.Println("4", err)
-			return
-		}
-		fmt.Println("Result of InsertOne:")
-		fmt.Println(insertOneResult.InsertedID)
-
-		c.JSON(http.StatusOK, gin.H{
-			"AccessToken":  token,
-			"RefreshToken": base64.RawURLEncoding.EncodeToString([]byte(refreshToken)),
-		})
-	})
-
-	c.POST("/refresh", func(c *gin.Context) {
-		refreshToken := c.Request.Header.Get("RefreshToken")
-		if refreshToken == "" {
-			c.JSON(http.StatusBadRequest, gin.H{"error": "RefreshToken is empty"})
-			return
-		}
-		userJti, err := tokens.ParseRefreshToken(refreshToken)
-		if err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-			return
-		}
-		fmt.Println("userJti:", userJti)
-		var user structs.User
-		user, err = database.FindOne(client, ctx, dbName, collectionName, bson.M{"jti": userJti})
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-			return
-		}
-		if user.GUID == "" {
-			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid refresh token"})
-			return
-		}
-
-		if err := bcrypt.CompareHashAndPassword([]byte(user.RefreshToken), []byte(refreshToken)); err != nil {
-			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid refresh token"})
-			return
-		}
-
-		token, refreshToken, jti, err := tokens.GenerateTokens(user.GUID)
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-			return
-		}
-
-		hash, err := bcrypt.GenerateFromPassword([]byte(refreshToken), bcrypt.MinCost)
-		if err != nil {
-			log.Fatal(err)
-		}
-		user.RefreshToken = string(hash)
-		user.Jti = jti
-		update := bson.M{"$set": bson.M{"refreshToken": user.RefreshToken, "jti": user.Jti}}
-		updateResult, err := database.UpdateOne(client, ctx, dbName, collectionName, bson.M{"guid": user.GUID}, update)
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-			return
-		}
-		fmt.Println("Result of UpdateOne:")
-		fmt.Println(updateResult.UpsertedID)
-
-		c.JSON(http.StatusOK, gin.H{
-			"AccessToken":  token,
-			"RefreshToken": base64.RawURLEncoding.EncodeToString([]byte(refreshToken)),
-		})
-	})
+	c.GET("/:guid", controllers.GetTokens(c, envData, client, ctx))
+	c.POST("/refresh", controllers.Refresh(c, envData, client, ctx))
 
 	c.Run(":" + port)
 }
